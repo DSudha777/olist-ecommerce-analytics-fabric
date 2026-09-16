af2dff35d630feb064c81dbde44d8c86414812e7
DSudha777 <161975071+DSudha777@users.noreply.github.com>
2026-09-16 08:53:51 -0400

    Create dim_product table

--- sql/gold/dim_products.sql
+++ sql/gold/dim_products.sql
@@ -1,1 +1,11 @@
 
+CREATE TABLE dbo.dim_product
+AS
+SELECT
+    product_id,
+    product_category_name,
+    product_weight_g,
+    product_length_cm,
+    product_height_cm,
+    product_width_cm
+FROM Olist_Lakehouse.dbo.dim_product;
